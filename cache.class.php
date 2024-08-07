214dd5f0e792030be43e1119b4d5a4a1f4202338
Scott Baker <scott@perturb.org>
2024-08-06 18:45:11 -0700

    Add some comments

--- cache.class.php
+++ cache.class.php
@@ -3,16 +3,16 @@
 ////////////////////////////////////////////////////////
 
 class Cache {
-	public $pdo     = null;
-	public $db_file = "";
-	public $mode    = "";
+	public $pdo     = null; // PDO Object
+	public $db_file = "";   // Path to the SQLite file
+	public $mode    = "";   // 'json', 'igb', 'msgp'
 
 	public function __construct($opts = []) {
 		$this->db_file = $opts['db_file'] ?? "/dev/shm/cache.sqlite";
-		$is_new        = !file_exists($this->db_file);
+		$missing_db    = !file_exists($this->db_file);
 		$dsn           = "sqlite:" . $this->db_file;
-		$this->pdo     = new PDO($dsn);
 
+		$this->pdo  = new PDO($dsn);
 		$this->mode = $opts['mode'] ?? "";
 
 		if ($this->mode) {
@@ -27,17 +27,16 @@ class Cache {
 			die("No serialization formats available");
 		}
 
-		if ($is_new) {
+		if ($missing_db) {
 			$ok = $this->init_db();
 		}
 
 		return $this->pdo;
 	}
 
-	public function __destruct() {
+	public function __destruct() { }
 
-	}
-
+	// Create/Erase the database structure
 	public function init_db() {
 		$sql = "DROP TABLE IF EXISTS cache;";
 		$ok  = $this->pdo->exec($sql);
@@ -48,11 +47,107 @@ class Cache {
 			Key VARCHAR(255) PRIMARY Key UNIQUE,
 			Value BLOB
 		);";
-		$ok  = $this->pdo->exec($sql);
 
+		$ok  = $this->pdo->exec($sql);
 		chmod($this->db_file, 0666);
 
 		return $ok;
+	}
+
+	// Read an item from the cache
+	public function get($key) {
+		$sql = "SELECT rowid, Value, ExpireTime FROM cache WHERE Key = ?;";
+
+		try {
+			$sth = $this->pdo->prepare($sql);
+		} catch (PDOException $e) {
+			$code = $e->getCode();
+			if ($code === "HY000") {
+				$this->error_out("Table 'cache' missing. DB corrupt?", 84203);
+			} else {
+				$this->error_out($e->getMessage(), 12352);
+			}
+
+		}
+
+		$ok  = $sth->execute([$key]);
+		$ret = $sth->fetch(PDO::FETCH_NUM);
+
+		$rowid  = $ret[0] ?? -1;
+		$data   = $ret[1] ?? "";
+		$expire = $ret[2] ?? 0;
+		$now    = time();
+
+		// If it's expired we remove it from the DB and return null
+		if ($now > $expire) {
+			$this->delete($key);
+			$ret = null;
+		} else {
+			$ret = $this->unpack($data);
+		}
+
+		return $ret;
+	}
+
+	// Delete an item from the cache
+	public function delete($key) {
+		$sql = "DELETE FROM cache WHERE Key = ?;";
+
+		$sth = $this->pdo->prepare($sql);
+		$ok  = $sth->execute([$key]);
+
+		return $ok;
+	}
+
+	// Write/Replace a cache entry into the cache
+	public function set($key, $value, $expires = 0) {
+		if (empty($expires)) {
+			$expires = time() + 3600;
+		}
+
+		$sql    = "REPLACE INTO cache (Key, Value, ExpireTime, CreateTime) VALUES (:key, :value, :expires, :create);";
+		$create = time();
+
+		$enc = $this->pack($value);
+
+		$sth = $this->pdo->prepare($sql);
+		$sth->bindParam(":key"    , $key    , PDO::PARAM_STR);
+		$sth->bindParam(":value"  , $enc    , PDO::PARAM_LOB);
+		$sth->bindParam(":expires", $expires, PDO::PARAM_INT);
+		$sth->bindParam(":create" , $create , PDO::PARAM_INT);
+		$ok  = $sth->execute();
+
+		return $ok;
+	}
+
+	// Serialize object
+	private function pack($str) {
+		if ($this->mode === "igb") {
+			$ret = igbinary_serialize($str);
+		} elseif ($this->mode === "msgp") {
+			$ret = msgpack_pack($str);
+		} elseif ($this->mode === "json") {
+			$ret = json_encode($str);
+		} else {
+			$ret = false;
+		}
+
+		return $ret;
+	}
+
+	// Unserialize object
+	private function unpack($str) {
+		if ($this->mode === "igb") {
+			$ret = igbinary_unserialize($str);
+		} elseif ($this->mode === "msgp") {
+			$ret = msgpack_unpack($str);
+		} elseif ($this->mode === "json") {
+			$ret = json_decode($str, true);
+		} else {
+			$ret = false;
+		}
+
+		return $ret;
 	}
 
 	// Spit out an error message
@@ -126,97 +221,6 @@ class Cache {
 		print $out;
 
 		exit;
-	}
-
-	public function get($key) {
-		$sql = "SELECT rowid, Value, ExpireTime FROM cache WHERE Key = ?;";
-
-		try {
-			$sth = $this->pdo->prepare($sql);
-		} catch (PDOException $e) {
-			$code = $e->getCode();
-			if ($code === "HY000") {
-				$this->error_out("Table 'cache' missing. DB corrupt?", 84203);
-			} else {
-				$this->error_out($e->getMessage(), 12352);
-			}
-
-		}
-
-		$ok  = $sth->execute([$key]);
-		$ret = $sth->fetch(PDO::FETCH_NUM);
-
-		$rowid  = $ret[0] ?? -1;
-		$data   = $ret[1] ?? "";
-		$expire = $ret[2] ?? 0;
-		$now    = time();
-
-		// If it's expired we remove it from the DB and return null
-		if ($now > $expire) {
-			$this->delete($key);
-			$ret = null;
-		} else {
-			$ret = $this->unpack($data);
-		}
-
-		return $ret;
-	}
-
-	public function delete($key) {
-		$sql = "DELETE FROM cache WHERE Key = ?;";
-
-		$sth = $this->pdo->prepare($sql);
-		$ok  = $sth->execute([$key]);
-
-		return $ok;
-	}
-
-	public function set($key, $value, $expires = 0) {
-		if (empty($expires)) {
-			$expires = time() + 3600;
-		}
-
-		$sql    = "REPLACE INTO cache (Key, Value, ExpireTime, CreateTime) VALUES (:key, :value, :expires, :create);";
-		$create = time();
-
-		$enc = $this->pack($value);
-
-		$sth = $this->pdo->prepare($sql);
-		$sth->bindParam(":key"    , $key    , PDO::PARAM_STR);
-		$sth->bindParam(":value"  , $enc    , PDO::PARAM_LOB);
-		$sth->bindParam(":expires", $expires, PDO::PARAM_INT);
-		$sth->bindParam(":create" , $create , PDO::PARAM_INT);
-		$ok  = $sth->execute();
-
-		return $ok;
-	}
-
-	private function pack($str) {
-		if ($this->mode === "igb") {
-			$ret = igbinary_serialize($str);
-		} elseif ($this->mode === "msgp") {
-			$ret = msgpack_pack($str);
-		} elseif ($this->mode === "json") {
-			$ret = json_encode($str);
-		} else {
-			$ret = false;
-		}
-
-		return $ret;
-	}
-
-	private function unpack($str) {
-		if ($this->mode === "igb") {
-			$ret = igbinary_unserialize($str);
-		} elseif ($this->mode === "msgp") {
-			$ret = msgpack_unpack($str);
-		} elseif ($this->mode === "json") {
-			$ret = json_decode($str, true);
-		} else {
-			$ret = false;
-		}
-
-		return $ret;
 	}
 }
 
